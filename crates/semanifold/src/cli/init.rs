60e9c2d87f628bad804f64db690e10833364540f
MoreKindness <f2248472180@outlook.com>
2025-08-19 21:17:27 +0800

    refactor: fmt code

--- crates/semanifold/src/cli/init.rs
+++ crates/semanifold/src/cli/init.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use clap::{arg, Args};
+use clap::{Args, arg};
 use log::info;
 
 #[derive(Debug, Args)]
@@ -9,27 +9,41 @@ pub(crate) struct Init {
     pub root: PathBuf,
     #[arg(short, long, default_value = "true")]
     pub tag: bool,
-
 }
 
 pub(crate) fn run(init: &Init) -> anyhow::Result<()> {
     std::fs::create_dir_all(&init.root)?;
 
-    // 初始化config.toml文件
+    // init config.toml file
     let config_path = init.root.join("config.toml");
 
     let mut table = toml::Table::new();
-    // 初始化packages
+    // init packages
     let packages = toml::Table::new();
     table.insert("packages".to_string(), toml::Value::Table(packages));
-    // 初始化tags
+    // init tags
     let mut tags = toml::Table::new();
     if init.tag {
-        tags.insert("feat".to_string(), toml::Value::String("New Feature".to_string()));
-        tags.insert("fix".to_string(), toml::Value::String("Bug Fix".to_string()));
-        tags.insert("chore".to_string(), toml::Value::String("Chore".to_string()));
-        tags.insert("refactor".to_string(), toml::Value::String("Refactor".to_string()));
-        tags.insert("perf".to_string(), toml::Value::String("Performance Improvement".to_string()));
+        tags.insert(
+            "feat".to_string(),
+            toml::Value::String("New Feature".to_string()),
+        );
+        tags.insert(
+            "fix".to_string(),
+            toml::Value::String("Bug Fix".to_string()),
+        );
+        tags.insert(
+            "chore".to_string(),
+            toml::Value::String("Chore".to_string()),
+        );
+        tags.insert(
+            "refactor".to_string(),
+            toml::Value::String("Refactor".to_string()),
+        );
+        tags.insert(
+            "perf".to_string(),
+            toml::Value::String("Performance Improvement".to_string()),
+        );
     }
     table.insert("tags".to_string(), toml::Value::Table(tags));
 

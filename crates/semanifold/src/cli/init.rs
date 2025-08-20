263db06628d3187634ee4a37301f898a72dad6fd
MoreKindness <f2248472180@outlook.com>
2025-08-20 17:26:05 +0800

    feat: Introduce i18n text after the init command completes

--- crates/semanifold/src/cli/init.rs
+++ crates/semanifold/src/cli/init.rs
@@ -2,6 +2,7 @@ use std::{fmt::Debug, path::PathBuf};
 
 use clap::{Args, arg};
 use log::info;
+use rust_i18n::t;
 use toml_edit::{DocumentMut, Table, value};
 
 #[derive(Debug, Args)]
@@ -32,6 +33,6 @@ pub(crate) fn run(init: &Init) -> anyhow::Result<()> {
     doc["tags"] = toml_edit::Item::Table(tags);
     // write config.toml file
     std::fs::write(config_path, doc.to_string())?;
-    info!("Initialized semanifold in {}", init.root.display());
+    info!("{} {}", t!("cli.init.finish"), init.root.display());
     Ok(())
 }

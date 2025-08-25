81711c73a21e3edff7953a9caa318f520b32b5e7
MoreKindness <f2248472180@outlook.com>
2025-08-25 11:06:46 +0800

    fix: handle panic when ysy_locale fails to get the language

--- crates/semanifold/src/i18n.rs
+++ crates/semanifold/src/i18n.rs
@@ -1,5 +1,7 @@
 pub fn init() {
-    sys_locale::get_locale().map(|locale| {
-        rust_i18n::set_locale(&locale);
-    });
+    let Some(locale) = sys_locale::get_locale() else {
+        rust_i18n::set_locale("en");
+        return;
+    };
+    rust_i18n::set_locale(&locale);
 }
